590ad9e85d4519bed954554313f4bf94243b370b
Alexander Kizilov <kizilov.alex1823@gmail.com>
2023-04-05 15:26:41 +0300

    init

---
+++ src/services/AuthService.ts
@@ -0,0 +1,29 @@
+import { IAuth } from '../models/Auth'
+
+class AuthService {
+  constructor() {}
+
+  async create(data: IAuth) {
+    
+  }
+
+  async getAll() {
+    
+  }
+
+  async getOnce(id: string) {
+
+  }
+
+  async update(id: string, data: Partial<IAuth>) {
+
+  }
+
+  async delete(id: string) {
+
+  }
+}
+
+const authService = new AuthService()
+
+export default authService
---
+++ src/services/ShortService.ts
@@ -0,0 +1,29 @@
+import { IShort } from '../models/Short'
+
+class ShortService {
+  constructor() {}
+
+  async create(data: IShort) {
+    
+  }
+
+  async getAll() {
+    
+  }
+
+  async getOnce(id: string) {
+
+  }
+
+  async update(id: string, data: Partial<IShort>) {
+
+  }
+
+  async delete(id: string) {
+
+  }
+}
+
+const shortService = new ShortService()
+
+export default shortService
